1b1bd14044b1cc64ed9dd23286cb23b33ae404aa
Quinn Rodney <followthewhiterabit93@gmail.com>
2024-06-20 13:32:45 -0500

    Add files via upload

---
+++ inputoutput/BasicOutput.java
@@ -0,0 +1,18 @@
+package inputoutput;
+
+public class BasicOutput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Java programming is fun!");
+		
+		int a = 4;
+		int b = 6;
+		int c = a + b;
+		
+		System.out.printf("%d + %d = %d", a,b,c);
+		
+	}
+
+}
