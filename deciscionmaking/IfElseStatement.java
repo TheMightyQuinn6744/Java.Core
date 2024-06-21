4aea62b9bf9be95233186b97545a2e18adf7f3e5
Quinn Rodney <followthewhiterabit93@gmail.com>
2024-06-21 18:15:02 -0500

    Create IfElseStatement.java

---
+++ deciscionmaking/IfElseStatement.java
@@ -0,0 +1,23 @@
+package deciscionmaking;
+
+import java.util.Scanner;
+
+public class IfElseStatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner inputNumber = new Scanner(System.in);
+		System.out.println("Please enter a number : ");
+		int number = inputNumber.nextInt();
+		if (number % 2 == 0) {
+			System.out.println("Number is even");
+	} 
+	else {
+		System.out.println("Number is odd");
+		
+	}
+	inputNumber.close();
+
+	}
+}
