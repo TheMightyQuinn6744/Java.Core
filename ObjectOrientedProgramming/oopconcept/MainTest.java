e0aeb2b90be14711ff1aa49d06d96b6f93c260ae
Quinn Rodney <followthewhiterabit93@gmail.com>
2024-06-21 17:32:45 -0500

    Create MainTest.java

---
+++ ObjectOrientedProgramming/oopconcept/MainTest.java
@@ -0,0 +1,22 @@
+package oopconcept;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		Person person = new Person("David", 60);
+//		
+//		person.wakeUp();
+//		person.showWeight();
+//		person.eat();
+//		person.showWeight();
+//		person.doexercise();
+//		person.showWeight();
+		
+		//toString()
+		Person person2 = new Person("David", 30, 60, 2000);
+		System.out.println(person2);
+	}
+
+}
